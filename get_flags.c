98054a1047f18a5a91317f4ee0ad28de4e637f92
CandiceCoding <candicenonomasimini@gmail.com>
2023-07-25 11:57:11 -0700

    My get flags

--- get_flags.c
+++ get_flags.c
@@ -2,8 +2,8 @@
 
 /**
  * get_flags - Calculates active flags
- * @format: Formatted string in which to print the arguments
- * @i: take a parameter.
+ * @format: Formats string in which to print the arguments
+ * @i: take an active parameter.
  * Return: Flags:
  */
 int get_flags(const char *format, int *i)
