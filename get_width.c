f17e21ea25486cef9b4e5d43ff13cada372fccee
CandiceCoding <candicenonomasimini@gmail.com>
2023-07-25 10:52:14 -0700

    My get width

--- get_width.c
+++ get_width.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * get_width - Calculates the width for printing result.
- * @i: List all arguments to be printed.
- * @format: Formats string in which it prints the arguments.
- * @list: list of all arguments.
+ * get_width - Calculates the width for printing
+ * @format: Formatted string in which to print the arguments.
+ * @i: List of arguments to be printed.
+ * @list: list of arguments.
  *
  * Return: width.
  */
