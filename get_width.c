b0aaf870b62074a26b52c80e51d6c6b5f5f89382
CandiceCoding <candicenonomasimini@gmail.com>
2023-07-25 11:01:49 -0700

    My get_width

--- get_width.c
+++ get_width.c
@@ -16,14 +16,12 @@ int get_width(const char *format, int *i, va_list list)
 	if (format[curr_i] != '.')
 		return (width);
 
-	width = 0;
-
 	for (curr_i = i + 1; format[curr_i] != '\0'; curr_i++)
 	{
 		if (is_digit(format[curr_i]))
 		{
-			width * = 10;
-			width + = format[curr_i] - '0';
+			width *= 10;
+			width += format[curr_i] - '0';
 		}
 		else if (format[curr_i] == '*')
 		{
